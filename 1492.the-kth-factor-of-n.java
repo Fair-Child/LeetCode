096bfb84b622644c4a350700fb3ce8a3adf2fa36
Brent <brentliang94@gmail.com>
2022-03-25 18:16:57 -0400

    new solutions

---
+++ 1492.the-kth-factor-of-n.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1492 lang=java
+ *
+ * [1492] The kth Factor of n
+ */
+
+// @lc code=start
+class Solution {
+    public int kthFactor(int n, int k) {
+        
+        for (int i = 1; i < n / 2 + 1; i++) {
+            if (n % i == 0) {
+                k--;
+                if (k == 0) {
+                    return i;
+                }
+            }
+        }
+
+        return k == 1? n : -1;
+    }
+}
+// @lc code=end
+
