92a1c092981b08bf92927aad2f711c9e079de1f7
Brent <brentliang94@gmail.com>
2022-04-02 23:17:11 -0400

    solution modified

--- 1268.search-suggestions-system.java
+++ 1268.search-suggestions-system.java
@@ -11,29 +11,18 @@ import java.util.TreeMap;
 
 // @lc code=start
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> res = new ArrayList<>();
-        TreeMap<String, Integer> map = new TreeMap<>();
-        Arrays.sort(products);
-        List<String> productsList = Arrays.asList(products);
 
-        for (int i = 0; i < products.length; i++) {
-            map.put(products[i], i);
-        }
+    class Node {
+        boolean isWord = false;
+        List<Node> children = Arrays.asList(new Node[26]);
+    }
 
-        String key = "";
-        for (char c : searchWord.toCharArray()) {
-            key += c;
-            String ceiling = map.ceilingKey(key);
-            String floor = map.floorKey(key + "~");
-            if (ceiling == null || floor == null) break;
-            res.add(productsList.subList(map.get(ceiling), Math.min(map.get(ceiling) + 3, map.get(floor) + 1)));
-        }
+    Node root, curr;
+    List<S
 
-        while (res.size() < searchWord.length()) {
-            res.add(new ArrayList<>());
-        }
-        return res;
+    
+
+    
     }
 }
 // @lc code=end
