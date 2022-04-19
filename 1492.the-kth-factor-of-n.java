cff3af8fb0f81ed444f4d74401c325b0f1a89b91
Brent <brentliang94@gmail.com>
2022-04-18 20:26:49 -0400

    new solutions added

--- 1492.the-kth-factor-of-n.java
+++ 1492.the-kth-factor-of-n.java
@@ -8,7 +8,7 @@
 class Solution {
     public int kthFactor(int n, int k) {
         
-        for (int i = 1; i < n / 2 + 1; i++) {
+        for (int i = 1; i <= n / 2; i++) {
             if (n % i == 0) {
                 k--;
                 if (k == 0) {
