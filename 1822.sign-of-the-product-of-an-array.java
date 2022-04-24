0bf25c18c8cbd0049d8b854e82fe01eaed36c76b
Brent <brentliang94@gmail.com>
2022-04-23 22:43:27 -0400

    new solutions

--- 1822.sign-of-the-product-of-an-array.java
+++ 1822.sign-of-the-product-of-an-array.java
@@ -12,12 +12,10 @@ class Solution {
         int sign = 1;
 
         for (int num:nums) {
-            if (num == 0) {
+            if (num == 0)
                 return 0;
-            }
-            if (num < 0) {
+            if (num < 0)
                 sign = -sign;
-            }
         }
         return sign;
     }
