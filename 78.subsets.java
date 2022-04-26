74fd0b6bcaa318f1ac1eaa9b0c43ed7f4eaf6a7d
Brent <brentliang94@gmail.com>
2022-04-26 19:43:41 -0400

    new solutions

--- 78.subsets.java
+++ 78.subsets.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * @lc app=leetcode id=78 lang=java
  *
@@ -8,8 +10,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         
-        
+        List<List<Integer>> output = new ArrayList<>();
+        output.add(new ArrayList<>());
 
+        for (int num : nums) {
+            List<List<Integer>> subset = new ArrayList<>();
+            for (List<Integer> curr : output) {
+                subset.add(new ArrayList<Integer>(curr){{add(num);}});
+            }
+            for (List<Integer> curr : subset) {
+                output.add(curr);
+            }
+        }
+        return output;
     }
 }
 // @lc code=end
