c677045f071d0fb797c5a3500fb6297758adad24
Brent <brentliang94@gmail.com>
2022-06-02 16:40:34 -0400

    new solutions

---
+++ 1290.convert-binary-number-in-a-linked-list-to-integer.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1290 lang=java
+ *
+ * [1290] Convert Binary Number in a Linked List to Integer
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;
+        }
+
+        return num;
+    }
+}
+// @lc code=end
+
---
+++ 404.sum-of-left-leaves.java
@@ -0,0 +1,43 @@
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=404 lang=java
+ *
+ * [404] Sum of Left Leaves
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return processSubtree(root, false);
+    }
+
+    private int processSubtree(TreeNode subtree, boolean isLeft) {
+        if (subtree == null) {
+            return 0;
+        }
+
+        if (subtree.left == null && subtree.right == null) {
+            return isLeft? subtree.val : 0;
+        }
+
+        return processSubtree(subtree.left, true) + processSubtree(subtree.right, false);
+    }
+}
+// @lc code=end
+
