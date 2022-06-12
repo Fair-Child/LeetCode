966941ee678424f6db06de78f15f968fccaa2880
Fair-Child <brentliang94@gmail.com>
2022-06-12 18:15:17 -0400

    add new solution of 198

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -6,25 +6,48 @@
 
 // @lc code=start
 class Solution {
-    public int rob(int[] nums) {
-        int len = nums.length;
+    // public int rob(int[] nums) {
+    //     int len = nums.length;
 
-        // if no house, 0 profit
-        if (len == 0) {
+    //     // if no house, 0 profit
+    //     if (len == 0) {
+    //         return 0;
+    //     }
+
+    //     int[] maxProfit = new int[nums.length + 1];
+    //     maxProfit[len] = 0;
+    //     maxProfit[len - 1] = nums[len - 1];
+
+    //     for (int i = len - 2; i >= 0; i--) {
+    //         maxProfit[i] = Math.max(maxProfit[i + 1], maxProfit[i + 2] + nums[i]);
+    //     }
+
+    //     return maxProfit[0];
+
+    // }
+
+    public int rob(int[] nums) {
+        int length = nums.length;
+        
+        if (length == 0) {
             return 0;
         }
-
-        int[] maxProfit = new int[nums.length + 1];
-        maxProfit[len] = 0;
-        maxProfit[len - 1] = nums[len - 1];
-
-        for (int i = len - 2; i >= 0; i--) {
-            maxProfit[i] = Math.max(maxProfit[i + 1], maxProfit[i + 2] + nums[i]);
+        
+        int robNext, robNextOne;
+        
+        robNextOne = 0;
+        robNext = nums[length - 1];
+        
+        for (int i = length - 2; i >= 0; i--) {
+            int current = Math.max(robNext, robNextOne + nums[i]);
+            
+            robNextOne = robNext;
+            robNext = current;
         }
-
-        return maxProfit[0];
-
+        
+        return robNext;
     }
+
 }
 // @lc code=end
 
