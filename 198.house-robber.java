9157edd70b5fb42358dbaa598aa06ff05df9b87c
Fair-Child <brentliang94@gmail.com>
2022-06-14 10:39:13 -0400

    switch to solution one

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -6,48 +6,52 @@
 
 // @lc code=start
 class Solution {
+
+    // Solution One
     
-    // public int rob(int[] nums) {
-    //     int len = nums.length;
-
-    //     // if no house, 0 profit
-    //     if (len == 0) {
-    //         return 0;
-    //     }
-
-    //     int[] maxProfit = new int[nums.length + 1];
-    //     maxProfit[len] = 0;
-    //     maxProfit[len - 1] = nums[len - 1];
-
-    //     for (int i = len - 2; i >= 0; i--) {
-    //         maxProfit[i] = Math.max(maxProfit[i + 1], maxProfit[i + 2] + nums[i]);
-    //     }
-
-    //     return maxProfit[0];
-
-    // }
-
     public int rob(int[] nums) {
-        int length = nums.length;
-        
-        if (length == 0) {
+        int len = nums.length;
+
+        // if no house, 0 profit
+        if (len == 0) {
             return 0;
         }
-        
-        int robNext, robNextOne;
-        
-        robNextOne = 0;
-        robNext = nums[length - 1];
-        
-        for (int i = length - 2; i >= 0; i--) {
-            int current = Math.max(robNext, robNextOne + nums[i]);
-            
-            robNextOne = robNext;
-            robNext = current;
+
+        int[] maxProfit = new int[nums.length + 1];
+        maxProfit[len] = 0;
+        maxProfit[len - 1] = nums[len - 1];
+
+        for (int i = len - 2; i >= 0; i--) {
+            maxProfit[i] = Math.max(maxProfit[i + 1], maxProfit[i + 2] + nums[i]);
         }
-        
-        return robNext;
+
+        return maxProfit[0];
+
     }
+
+    // Solution Two
+
+    // public int rob(int[] nums) {
+    //     int length = nums.length;
+        
+    //     if (length == 0) {
+    //         return 0;
+    //     }
+        
+    //     int robNext, robNextOne;
+        
+    //     robNextOne = 0;
+    //     robNext = nums[length - 1];
+        
+    //     for (int i = length - 2; i >= 0; i--) {
+    //         int current = Math.max(robNext, robNextOne + nums[i]);
+            
+    //         robNextOne = robNext;
+    //         robNext = current;
+    //     }
+        
+    //     return robNext;
+    // }
 
 }
 // @lc code=end
