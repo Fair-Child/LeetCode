289e3ac8ba100199686c513cfd1d1698f5cc3cc3
HF <brentliang94@gmail.com>
2023-05-29 18:22:03 -0400

    added question 70 and 409

---
+++ 409.longest-palindrome.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=409 lang=java
+ *
+ * [409] Longest Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindrome(String s) {
+        
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
+
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
+
+    }
+}
+// @lc code=end
+
