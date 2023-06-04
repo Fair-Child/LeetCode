d338222978468d8cf214232d00005c5a9f7b7e82
HF <brentliang94@gmail.com>
2023-06-03 23:13:10 -0400

    added 203 and 383

--- 383.ransom-note.java
+++ 383.ransom-note.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * @lc app=leetcode id=383 lang=java
  *
@@ -11,19 +13,33 @@ class Solution {
     // Time Complexity: O
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] counter = new int[26];
+        // int[] counter = new int[26];
 
-        for (char c: magazine.toCharArray()) {
-            counter[c - 'a']++;
+        // for (char c: magazine.toCharArray()) {
+        //     counter[c - 'a']++;
+        // }
+
+        // for (char c: ransomNote.toCharArray()) {
+        //     if (counter[c - 'a'] == 0) {
+        //         return false;
+        //     }
+        //     counter[c - 'a']--;
+        // }
+
+        // return true;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for (char c: ransomNote.toCharArray()) {
-            if (counter[c - 'a'] == 0) {
+        for (char c : ransomNote.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) <= 0) {
                 return false;
             }
-            counter[c - 'a']--;
+            map.put(c, map.get(c) - 1);
         }
-
         return true;
     }
 
