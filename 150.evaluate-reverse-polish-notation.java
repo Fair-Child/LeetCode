e528293ccbcf523f403c6f7f4d2cea8c0dd34c44
HF <brentliang94@gmail.com>
2023-07-19 16:24:47 -0400

    added 150

--- 150.evaluate-reverse-polish-notation.java
+++ 150.evaluate-reverse-polish-notation.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /*
  * @lc app=leetcode id=150 lang=java
  *
@@ -7,6 +9,38 @@
 // @lc code=start
 class Solution {
     public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (!"+-*/".contains(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
+            }
+
+            int num2 = stack.pop();
+            int num1 = stack.pop();
+            int result = 0;
+
+            switch(token) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            stack.push(result);
+        }
+
+        return stack.pop();
         
     }
 }
